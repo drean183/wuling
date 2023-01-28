dc2d0b995a8d2874a128b4cd1d7a5efe793cbcaf
drean183 <christocok18@gmail.com>
2023-01-28 19:32:21 +0700

    js and css minify

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,34 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            maxSize: 70000,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            automaticNameDelimiter: '~',
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
+        minimizer: [new TerserPlugin()],
+    },
     module: {
         rules: [
             //babel
@@ -18,4 +44,7 @@ module.exports = merge(common, {
             },
         ],
     },
+    plugins: [
+        new CssMinimizerPlugin(),
+    ],
 })
